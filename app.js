17394062b5656ae3c8aebc6854f899cc00f6b10c
Atiq <cross.016x@gmail.com>
2023-12-22 14:47:51 +0600

    feat - Speacial Attack initiated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       palyerHealth: 100,
       monsterHealth: 100,
+      currentound: 0,
     };
   },
   computed: {
@@ -16,9 +17,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.palyerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return this.currentound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentound++;
       const attachValue = getRandomValue(5, 12);
       this.monsterHealth -= attachValue;
       this.attackPlayer();
@@ -26,6 +31,12 @@ const app = Vue.createApp({
     attackPlayer() {
       const attachValue = getRandomValue(8, 15);
       this.palyerHealth -= attachValue;
+    },
+    specialAttackMonster() {
+      this.currentound++;
+      const attachValue = getRandomValue(10, 25);
+      this.monsterHealth -= attachValue;
+      this.attackPlayer();
     },
   },
 });
