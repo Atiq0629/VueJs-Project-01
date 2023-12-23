30ec63bde049a285c2fdd36b14acd7524d3e209a
Atiq <cross.016x@gmail.com>
2023-12-23 11:28:13 +0600

    feat - Game Over Screen initiated.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
       palyerHealth: 100,
       monsterHealth: 100,
       currentound: 0,
+      winner: null,
     };
   },
   computed: {
@@ -19,6 +20,26 @@ const app = Vue.createApp({
     },
     mayUseSpecialAttack() {
       return this.currentound % 3 !== 0;
+    },
+  },
+  watch: {
+    palyerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // a draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.palyerHealth <= 0) {
+        // a draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // monster lost
+        this.winner = "player";
+      }
     },
   },
   methods: {
@@ -36,6 +57,16 @@ const app = Vue.createApp({
       this.currentound++;
       const attachValue = getRandomValue(10, 25);
       this.monsterHealth -= attachValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.palyerHealth + healValue > 100) {
+        this.palyerHealth = 100;
+      } else {
+        this.palyerHealth += healValue;
+      }
       this.attackPlayer();
     },
   },
